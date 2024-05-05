50a4909879302d9b33adda2f4aa51fad1784724a
Orin10120 <rindang3001@gmail.com>
2024-05-05 16:48:00 +0700

    Add project files.

---
+++ AljabarConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AljabarLibraries;
+
+namespace AljabarLibraries
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            double[] akar = Kuadrat.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+            Console.WriteLine("Akar dari persamaan kuadrat x^2 - 3x - 10 = 0 adalah {0} dan {1}",
+                akar[0], akar[1]);
+
+            double[] hasilKuadrat = Kuadrat.HasilKuadrat(new double[] { 2, -3 });
+            Console.WriteLine("Hasil kuadrat dari persamaan 2x - 3 adalah {0}x^2 + {1}x + {2}",
+                hasilKuadrat[0], hasilKuadrat[1], hasilKuadrat[2]);
+
+ 
+        }
+    }
+}
+
---
+++ AljabarLibraries/Kuadrat.cs
@@ -0,0 +1,37 @@
+﻿namespace AljabarLibraries
+{
+    public class Kuadrat
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+
+            double d = b * b - 4 * a * c;
+
+            if (d >= 0)
+            {
+                double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(d)) / (2 *a);
+
+                return new double[] { x1, x2 };
+            } else
+            {
+                return new double[] { 0, 0 };
+            }
+        }
+
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            double c = a * a;
+            double d = -2 * a * b;
+            double e = b * b;
+
+            return new double[] { c, d, e };
+        }
+    }
+}
